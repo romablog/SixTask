2d3dd82261697a6918325350738644784843a46b
romablog <iromablog@gmail.com>
2016-02-02 14:38:39 +0300

    structure

--- SixTask/views.py
+++ SixTask/views.py
@@ -1,2 +1,22 @@
+import sys
+from io import StringIO
+from django.http import JsonResponse
+
+simpleDict = {}
 
 
+def simpleFuction(request):
+    if request.method == 'GET':
+        print(request.GET)
+    elif request.method == 'POST':
+        print(request.POST)
+        code =  request.POST['data']
+        str = functionForExec(code)
+    return JsonResponse({'foo': str})
+
+def functionForExec(str):
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    exec(str)
+    sys.stdout = old_stdout
+    return (redirected_output.getvalue())
